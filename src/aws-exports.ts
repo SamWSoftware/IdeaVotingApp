5a70fe06fecce09e5d4e1694d7db3d311b3603d7
Sam Williams <sam@completecoding.io>
2022-07-05 18:12:22 +0200

    frontend app

---
+++ src/aws-exports.ts
@@ -0,0 +1,8 @@
+const awsConfig = {
+  aws_project_region: "eu-central-1",
+  aws_cognito_region: "eu-central-1",
+  aws_user_pools_id: "YOUR_USER_POOL_ID",
+  aws_user_pools_web_client_id: "YOUR_USER_POOL_CLIENT_ID",
+};
+
+export default awsConfig;
